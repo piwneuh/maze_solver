cca0a9137789f1a754fdbe9891d536e0126c5f8c
piwneuh <filip.pinjuh@gmail.com>
2023-02-03 22:30:07 +0100

    improved solver

--- src/main.rs
+++ src/main.rs
@@ -96,6 +96,7 @@ fn parse(file_path: &str, rows: usize, columns: usize) -> Vec<Vec<Node>>{
 		if row.chars().nth(12) == Some('1') && row.chars().nth(13) == Some('1'){
 			exit = 1;
 		};
+
 		let node = Node{ west: west, east:east, north:north, south:south, key:key, exit:exit};
 		maze_matrix[index / 9][index % 9] = node;
 	}
@@ -103,13 +104,10 @@ fn parse(file_path: &str, rows: usize, columns: usize) -> Vec<Vec<Node>>{
 	maze_matrix
 }
 
-fn solve(maze: &Vec<Vec<Node>>) -> Option<Vec<(usize, usize)>> {
+fn solve(maze: &mut Vec<Vec<Node>>, start_x: usize, start_y: usize) -> Option<Vec<(usize, usize)>> {
 	let path = Vec::new();
-	let mut visited = vec![vec![false; maze.len()]; maze[0].len()];
+	let mut visited = vec![vec![false; maze[0].len()]; maze.len()];
 	let mut stack = VecDeque::new();
-
-	let start_x = 0;
-	let start_y = 0;
 
 	let mut key_inventory = 0;
 
@@ -117,7 +115,7 @@ fn solve(maze: &Vec<Vec<Node>>) -> Option<Vec<(usize, usize)>> {
 	while let Some((x, y, curr_path)) = stack.pop_back() {
 		visited[x][y] = true;
 
-		let node = &maze[x][y];
+		let mut node = maze[x][y];
 
 		if node.exit == 1 {
 			// Sanity yet
@@ -129,60 +127,95 @@ fn solve(maze: &Vec<Vec<Node>>) -> Option<Vec<(usize, usize)>> {
 		}
 
 		if node.key == 1 {
+			// Pick up key
 			key_inventory += 1;
-			// let mut new_path = curr_path.clone();
-			// new_path.push((x, y));
-			// stack.push_back((x, y, new_path));
+			println!("Key picked up, {} in inventory ! ({}, {})", key_inventory, x, y);
+			node.key = 0;
+			maze[x][y] = node;
+			
+			let mut new_path = curr_path.clone();
+			new_path.push((x, y));
+			stack.push_back((x, y, new_path));
 		}
 
-		if node.north == 0 && !visited[x - 1][y] {
+		if x == 0{
+			//println!("We reached the edge! ({}, {})", x, y);
+		}
+		else if node.north == 0 && !visited[x - 1][y] {
 			let mut new_path = curr_path.clone();
 			new_path.push((x, y));
 			stack.push_back((x - 1, y, new_path));
 		}
 
-		if node.south == 0 && !visited[x + 1][y] {
+		if x >= 5{
+			//println!("We reached the edge! ({}, {})", x, y);
+		}
+		else if node.south == 0 && !visited[x + 1][y] {
 			let mut new_path = curr_path.clone();
 			new_path.push((x, y));
 			stack.push_back((x + 1, y, new_path));
 		}
 
-		if node.west == 0 && !visited[x][y - 1] {
+		if y == 0{
+			//println!("We reached the edge! ({}, {})", x, y);
+		}
+		else if node.west == 0 && !visited[x][y - 1] {
 			let mut new_path = curr_path.clone();
 			new_path.push((x, y));
 			stack.push_back((x, y - 1, new_path));
 		}
 
-		if node.east == 0 && !visited[x][y + 1] {
+		if y >= 5{
+			//println!("We reached the edge! ({}, {})", x, y);
+		}
+		else if node.east == 0 && !visited[x][y + 1] {
 			let mut new_path = curr_path.clone();
 			new_path.push((x, y));
 			stack.push_back((x, y + 1, new_path));
 		}
+	
 		
 		// Open doors
 		if node.north == 2 && key_inventory > 0 && !visited[x - 1][y] {
+			// Remove door
+			println!("Opened door ! ({}, {})", x, y);
 			key_inventory -= 1;
+			node.north = 0;
+			maze[x][y] = node;
+
 			let mut new_path = curr_path.clone();
 			new_path.push((x, y));
 			stack.push_back((x - 1, y, new_path));
 		}
 
-		if node.south == 2 && key_inventory > 0 && !visited[x + 1][y] {
+		if node.south == 2 && key_inventory > 0 && !visited[x + 1][y]{
+			println!("Opened door ! ({}, {})", x, y);
 			key_inventory -= 1;
+			node.south = 0;
+			maze[x][y] = node;
+
 			let mut new_path = curr_path.clone();
 			new_path.push((x, y));
 			stack.push_back((x + 1, y, new_path));
 		}
 
-		if node.west == 2 && key_inventory > 0 && !visited[x][y - 1] {
+		if node.west == 2 && key_inventory > 0 && !visited[x][y - 1]{
+			println!("Opened door ! ({}, {})", x, y);
 			key_inventory -= 1;
+			node.west = 0;
+			maze[x][y] = node;
+
 			let mut new_path = curr_path.clone();
 			new_path.push((x, y));
 			stack.push_back((x, y - 1, new_path));
 		}
 
-		if node.east == 2 && key_inventory > 0 && !visited[x][y + 1] {
+		if node.east == 2 && key_inventory > 0 && !visited[x][y + 1]{
+			println!("Opened door ! ({}, {})", x, y);
 			key_inventory -= 1;
+			node.east = 0;
+			maze[x][y] = node;
+
 			let mut new_path = curr_path.clone();
 			new_path.push((x, y));
 			stack.push_back((x, y + 1, new_path));
@@ -199,9 +232,13 @@ fn main() {
 	// Set maze size
 	let rows = 9;
 	let columns = 6;
+
+	// Set maze start
+	let start_x = 0;
+	let start_y = 0;
 	
 	// Parsing
-	let maze: Vec<Vec<Node>> = parse(file_path, rows, columns);
+	let mut maze: Vec<Vec<Node>> = parse(file_path, rows, columns);
 
 	// Maze sanity print
 	// for row in &maze {
@@ -210,8 +247,8 @@ fn main() {
     //     }
     //     println!("_____________");
     // }
-	
-	let final_path = solve(&maze);
+
+	let final_path = solve(&mut maze, start_x, start_y);
 
 	for field in final_path{
 		print!("{:?}", field);
